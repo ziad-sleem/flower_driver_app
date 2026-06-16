eeb8986cf0f3756a48fdd5b5855917aee949c668
Ziad Sleem <ziad.sleem.111@gmail.com>
2026-06-16 16:15:07 +0300

    feat: implement localization constants and translation files for app modules

--- lib/core/localization_constants/error_massage_constants.dart
+++ lib/core/localization_constants/error_massage_constants.dart
@@ -3,13 +3,13 @@ import 'package:easy_localization/easy_localization.dart';
 abstract class ErrorConstants {
   const ErrorConstants._();
 
-  static String get noInternet => "".tr();
-  static String get connectionTimeout => "".tr();
-  static String get sendTimeout => "".tr();
-  static String get receiveTimeout => "".tr();
-  static String get serverError => "".tr();
-  static String get unexpectedError => "".tr();
-  static String get unknownError => "".tr();
-  static String get loginSuccessfully => "".tr();
-  static String get signupSuccessfully => "".tr();
+  static String get noInternet => "errors.no_internet".tr();
+  static String get connectionTimeout => "errors.connection_timeout".tr();
+  static String get sendTimeout => "errors.send_timeout".tr();
+  static String get receiveTimeout => "errors.receive_timeout".tr();
+  static String get serverError => "errors.server_error".tr();
+  static String get unexpectedError => "errors.unexpected_error".tr();
+  static String get unknownError => "errors.unknown_error".tr();
+  static String get loginSuccessfully => "errors.login_successfully".tr();
+  static String get signupSuccessfully => "errors.signup_successfully".tr();
 }
