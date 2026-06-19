e0f1337ddba93b469a6a3694067a0bcfd1a18ee1
Omar Ameen <omarxwork10@gmail.com>
2026-06-19 14:58:32 +0300

    refactor: comment out FcmService implementation for future use

--- lib/core/notifications/fcm_service.dart
+++ lib/core/notifications/fcm_service.dart
@@ -1,71 +1,71 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:tracking_app/core/notifications/local_notification_service.dart';
-import 'package:injectable/injectable.dart';
-import 'package:permission_handler/permission_handler.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:tracking_app/core/notifications/local_notification_service.dart';
+// import 'package:injectable/injectable.dart';
+// import 'package:permission_handler/permission_handler.dart';
 
-@lazySingleton
-class FcmService {
-  final FirebaseMessaging _messaging = FirebaseMessaging.instance;
+// @lazySingleton
+// class FcmService {
+//   final FirebaseMessaging _messaging = FirebaseMessaging.instance;
 
-  final LocalNotificationService _localNotificationService;
+//   final LocalNotificationService _localNotificationService;
 
-  FcmService(this._localNotificationService);
+//   FcmService(this._localNotificationService);
 
-  Future<void> initialize() async {
-    await _requestPermissions();
+//   Future<void> initialize() async {
+//     await _requestPermissions();
 
-    await _setupForegroundOptions();
+//     await _setupForegroundOptions();
 
-    _listenToNotifications();
+//     _listenToNotifications();
 
-    _listenToTokenRefresh();
+//     _listenToTokenRefresh();
 
-    await _handleInitialMessage();
+//     await _handleInitialMessage();
 
-    await _getDeviceToken();
-  }
+//     await _getDeviceToken();
+//   }
 
-  Future<void> _requestPermissions() async {
-    await _messaging.requestPermission(alert: true, badge: true, sound: true);
+//   Future<void> _requestPermissions() async {
+//     await _messaging.requestPermission(alert: true, badge: true, sound: true);
 
-    if (await Permission.notification.isDenied) {
-      await Permission.notification.request();
-    }
-  }
+//     if (await Permission.notification.isDenied) {
+//       await Permission.notification.request();
+//     }
+//   }
 
-  Future<void> _setupForegroundOptions() async {
-    await FirebaseMessaging.instance
-        .setForegroundNotificationPresentationOptions(
-          alert: true,
-          badge: true,
-          sound: true,
-        );
-  }
+//   Future<void> _setupForegroundOptions() async {
+//     await FirebaseMessaging.instance
+//         .setForegroundNotificationPresentationOptions(
+//           alert: true,
+//           badge: true,
+//           sound: true,
+//         );
+//   }
 
-  void _listenToNotifications() {
-    FirebaseMessaging.onMessage.listen((message) {
-      final notification = message.notification;
+//   void _listenToNotifications() {
+//     FirebaseMessaging.onMessage.listen((message) {
+//       final notification = message.notification;
 
-      if (notification != null) {
-        _localNotificationService.showNotification(
-          title: notification.title,
-          body: notification.body,
-        );
-      }
-    });
+//       if (notification != null) {
+//         _localNotificationService.showNotification(
+//           title: notification.title,
+//           body: notification.body,
+//         );
+//       }
+//     });
 
-    FirebaseMessaging.onMessageOpenedApp.listen((message) {});
-  }
+//     FirebaseMessaging.onMessageOpenedApp.listen((message) {});
+//   }
 
-  Future<void> _handleInitialMessage() async {
-    await _messaging.getInitialMessage();
-  }
+//   Future<void> _handleInitialMessage() async {
+//     await _messaging.getInitialMessage();
+//   }
 
-  Future<String?> _getDeviceToken() async {
-    return _messaging.getToken();
-  }
+//   Future<String?> _getDeviceToken() async {
+//     return _messaging.getToken();
+//   }
 
-  void _listenToTokenRefresh() {
-    _messaging.onTokenRefresh.listen((token) {});
-  }
-}
+//   void _listenToTokenRefresh() {
+//     _messaging.onTokenRefresh.listen((token) {});
+//   }
+// }
