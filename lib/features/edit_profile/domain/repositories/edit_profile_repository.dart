cd09ffb2b7e27400fbd9ba887266e66ee70c1aca
yassmena <>
2026-06-26 06:01:34 +0300

    feat: add edit profile and upload photo use cases with repository integration

---
+++ lib/features/edit_profile/domain/repositories/edit_profile_repository.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+import 'package:tracking_app/config/base/base_response.dart';
+import 'package:tracking_app/features/edit_profile/data/models/edit_profile_request.dart';
+import 'package:tracking_app/features/profile/domain/entities/driver_entity.dart';
+
+abstract interface class EditProfileRepository {
+  Future<BaseResponse<ProfileDriverEntity>> editProfile(
+    EditProfileParams params,
+  );
+  Future<BaseResponse<void>> uploadPhoto(File photo);
+}
