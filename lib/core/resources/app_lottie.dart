88d6b850a4264c56ce660e4cb9fc03ca9867f3c4
Omar Ameen <omarxwork10@gmail.com>
2026-08-24 17:12:56 +0300

    Refactor code structure for improved readability and maintainability

--- lib/core/resources/app_lottie.dart
+++ lib/core/resources/app_lottie.dart
@@ -1,5 +1,6 @@
 class AppLottie {
   AppLottie._();
   static const String succes = "assets/lottie/succeess.json";
-  static const onboarding = "assets/lottie/onboarding_animation.json";
+  static const String onboarding = "assets/lottie/onboarding_animation.json";
+  static const String splashAnimation = "assets/lottie/Delivery.json";
 }
